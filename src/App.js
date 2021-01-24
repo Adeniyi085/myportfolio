915e364e6e0728c7eff22e64f3179e823ecf48ff
exit <adeniyi.adegelu@gmail.com>
2021-01-24 13:10:17 +0100

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from "react-particles-js";
+import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Particles
+    className="particles-canvas"
+    params={{
+       particles:{
+         number:{
+           value:30,
+           density: {
+             enable: true,
+             value_area: 600
+           }
+         },
+         shape:{
+           type:  "square",
+           stroke:{
+             width:6,
+             color:"#f9ab00"
+           }
+         }
+       }
+    }}
+    />
     <Navbar />
-    </div>
+    <Header />
+    </>
   );
 }
 
